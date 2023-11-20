897c77fddeef1493316fa856c94e2b4a1e08a862
Kate <kate.wolfe7@gmail.com>
2023-11-19 19:59:20 -0500

    host

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://orca-app-az4mr.ondigitalocean.app/"]
+ALLOWED_HOSTS = ["https://orca-app-az4mr.ondigitalocean.app/"]
 
 
 # Application definition
